724913213b2a619e82d471fd759630d70ee8769c
flautarian <fgiacconi.p@gmail.com>
2022-05-04 00:40:53 +0200

    [WIP] Little fixes 2

--- src/main/java/edu/uoc/epcsd/showcatalog/controllers/ShowController.java
+++ src/main/java/edu/uoc/epcsd/showcatalog/controllers/ShowController.java
@@ -10,11 +10,14 @@ import javassist.tools.rmi.ObjectNotFoundException;
 import lombok.extern.log4j.Log4j2;
 import org.apache.kafka.common.errors.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.*;
 
+import javax.ws.rs.PathParam;
+import java.lang.reflect.Parameter;
 import java.sql.Time;
 import java.util.*;
 
@@ -39,12 +42,22 @@ public class ShowController {
         return showRepository.findAll();
     }
 
+    @GetMapping(path = "/consulta", produces = "application/json")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<List<Show>> consulta(@RequestParam(required = false) Long idShow, @RequestParam(required = false) String showName, @RequestParam(required = false) List<Long> idCategories) throws ObjectNotFoundException {
+        List<Show> showResults = null;
+        if(Objects.nonNull(idShow) || Objects.nonNull(showName) || Objects.nonNull(idCategories)){
+            showResults = Objects.nonNull(idShow) ? Arrays.asList(showRepository.findById(idShow).orElse(null)) :
+                    Objects.nonNull(showName) ? showRepository.findFirstByName(showName) :
+                            Objects.nonNull(idCategories) ? showRepository.findByCategoriesIdIn(idCategories) :
+                                    null;
+        }
+        return new ResponseEntity<>(showResults, HttpStatus.OK);
+    }
+
     @PostMapping("/crear")
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<Show> crear(@RequestBody Show show) {
-        log.trace("getAllCategories");
-        //if(Objects.isNull(show.getPerformances()))show.setPerformances(new ArrayList<Performance>());
-        //show.getPerformances().add(new Performance(new Date(), new Time(1,2,3), "url de prova", 100, "TANCAT"));
         processarCategories(show);
         Show showResult = showRepository.saveAndFlush(show);
         if(Objects.nonNull(showResult)){
@@ -55,6 +68,14 @@ public class ShowController {
         else throw new ResourceNotFoundException("operacio invalida, reviseu parametres");
     }
 
+    @DeleteMapping("/destruir/{idShow}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<String> destruir(@PathVariable Long idShow) {
+        log.trace("delete show : " + idShow);
+        showRepository.deleteById(idShow);
+        return new ResponseEntity<>("Show eliminat", HttpStatus.CREATED);
+    }
+
     @PostMapping("/{idShow}/crearActuacio")
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<Show> crear(@PathVariable Long idShow ,@RequestBody Performance performance) {
@@ -62,11 +83,23 @@ public class ShowController {
             Show show = showRepository.findById(idShow).orElse(null);
             if(Objects.nonNull(show)){
                 if(Objects.isNull(show.getPerformances()))show.setPerformances(new ArrayList<Performance>());
-                show.getPerformances().add(performance);
+                if(show.getPerformances().indexOf(performance) < 0)
+                    show.getPerformances().add(performance);
+                else
+                    throw new ResourceNotFoundException("operacio invalida, actuacio existent");
                 return new ResponseEntity<>(showRepository.saveAndFlush(show), HttpStatus.CREATED);
             } else throw new ResourceNotFoundException("operacio invalida, reviseu parametres");
         }
         else throw new ResourceNotFoundException("operacio invalida, reviseu parametres");
+    }
+
+    @GetMapping("/{idShow}/llistarActuacions")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<List<Performance>> llistarActuacions(@PathVariable("idShow") Long idShow) {
+        Show showResult = showRepository.findById(idShow).orElse(null);
+        if(Objects.nonNull(showResult))
+            return new ResponseEntity<>(showResult.getPerformances(), HttpStatus.OK);
+        return new ResponseEntity<>(null, HttpStatus.OK);
     }
 
     private void processarCategories(Show show) {
@@ -82,21 +115,22 @@ public class ShowController {
         });
     }
 
-    @DeleteMapping("/destruir/{idShow}")
+    @DeleteMapping("/{idShow}/destruirActuacio")
     @ResponseStatus(HttpStatus.OK)
-    public ResponseEntity<String> destruir(@PathVariable Long idShow) {
-        log.trace("delete show : " + idShow);
-        showRepository.deleteById(idShow);
-        return new ResponseEntity<>("Show eliminat", HttpStatus.CREATED);
-    }
-
-    @GetMapping(path = "/consulta/{idShow}", produces = "application/json")
-    @ResponseStatus(HttpStatus.OK)
-    public ResponseEntity<Show> consulta(@PathVariable Long idShow) throws ObjectNotFoundException {
-        log.trace("getting category : " + idShow);
-        Show showResult = showRepository.findById(idShow).orElse(null);
-        if(Objects.nonNull(showResult))return new ResponseEntity<>(showResult, HttpStatus.CREATED);
-        else throw new ObjectNotFoundException("Show no trobat");
+    public ResponseEntity<String> destruirActuacio(@PathVariable Long idShow, @RequestBody Performance performance) {
+        Show show = showRepository.findById(idShow).orElse(null);
+        log.trace("delete performance from: " + idShow);
+        if(Objects.nonNull(show) && Objects.nonNull(show.getPerformances())){
+            if(show.getPerformances().indexOf(performance) >= 0) {
+                show.getPerformances().remove(performance);
+                showRepository.saveAndFlush(show);
+            }
+            else {
+                return new ResponseEntity<>("operacio invalida, actuacio inexistent en el show proporcionat", HttpStatus.OK);
+            }
+            return new ResponseEntity<>("Actuacio eliminada", HttpStatus.OK);
+        }
+        return new ResponseEntity<>("operacio invalida, show o actuacio inexistents", HttpStatus.OK);
     }
 
 //    @GetMapping(path = "/shows/{idShow}", produces = "application/json")
